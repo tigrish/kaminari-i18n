2dcbdcc5b1096b7b2af0f6b0ec8d5733537b34f6
Christopher Dell <chris@tigrish.com>
2014-08-04 01:56:53 +0200

    Update to rspec's expect syntax

--- spec/kaminari-i18n_spec.rb
+++ spec/kaminari-i18n_spec.rb
@@ -1,8 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 Dir.glob('locales/*.yml').each do |locale_file|
-  describe "a kaminari-i18n #{locale_file} locale file" do
+  describe locale_file do
     it_behaves_like 'a valid locale file', locale_file
-    it { locale_file.should be_a_subset_of 'locales/en.yml' }
+    it { is_expected.to be_a_subset_of 'locales/en.yml' }
   end
 end
